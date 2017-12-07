f5b9871ef1617276a06a58918c668ce8da25fe70
FlyOceanFish <wangrifei1@126.com>
2017-12-07 17:32:19 +0800

    init

--- app/screens/components/SearchPage.js
+++ app/screens/components/SearchPage.js
@@ -8,13 +8,26 @@ import React, { Component } from 'react';
 import {
   View,
   WebView,
-  StyleSheet
+  StyleSheet,
+  Keyboard,
+  FlatList,
+  ActivityIndicator,
+  TouchableOpacity,
+  Text,
+  Dimensions
 } from 'react-native';
 
 import FavoriteDao from '../../dao/FavoriteDao'
 import {FLAG_STORAGE} from '../../dao/DataRepository'
+import LanguageDao,{FLAG_LANGUAGE} from '../../dao/LanguageDao'
 import CustomSearchView from '../view/CustomSearchView'
 import {Navigation} from 'react-native-navigation';
+import RepositoryCell from '../view/RepositoryCell'
+import ProjectModel from '../../model/ProjectModel'
+import Utils from '../../Vendor/Utils'
+
+const API_URL = 'https://api.github.com/search/repositories?q=';
+const QUERY_STR = '&sort=stars';
 
 Navigation.registerComponent('com.fof.CustomSearchView', () => CustomSearchView);
 
@@ -22,27 +35,63 @@ export default class SearchPage extends Component<{}> {
   constructor(props){
     super(props);
     this.state={
-      isFavorite:props.isFavorite
+      data:[],
+      favoriteKeys:[],
+      refresh:false,
+      showBottomButton:false
     }
+    this.rightButtonText = '搜索';
     this.favoriteDao = new FavoriteDao(props.flag);
+    this.languageDao = new LanguageDao(FLAG_LANGUAGE.flag_key);
     this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
     let icon = props.isFavorite?require('../../../img/ic_star.png'):require('../../../img/ic_unstar_transparent.png');
-    this.setNavigatorRightButtonImage(icon)
+    this.setNavigatorRightButton(this.rightButtonText);
   }
-  componentDidMount(){
+  componentWillMount(){
       this.props.navigator.setStyle({
       navBarCustomView: 'com.fof.CustomSearchView',
       navBarComponentAlignment: 'center',
-      navBarCustomViewInitialProps: {title: 'Hi Custom',aa:(button)=>{
-
+      navBarCustomViewInitialProps: {title: 'Hi Custom',textChange:(text,textInput)=>{
+        this.textInput = textInput;
+        this.inputText = text;
+      },onSubmitEditing:(text)=>{
+        this.onLoad();
       }}
     });
+    this.initKeys();
   }
-  setNavigatorRightButtonImage(icon){
+  saveKey(){
+    let key = this.inputText;
+    if (this.checkKeyIsExist(this.keys,key)) {
+      console.log(key+'已经存在');
+    }else {
+      key={
+          "path": key,
+          "name": key,
+          "checked": true
+        }
+        this.keys.unshift(key);
+        this.languageDao.save(this.keys);
+        console.log('保存成功');
+    }
+  }
+  async initKeys(){
+    this.keys = await this.languageDao.fetch();
+  }
+  // 检查key是否存在于keys
+  checkKeyIsExist(keys,key){
+    for (var i = 0; i < keys.length; i++) {
+      if (key.toLowerCase()===keys[i].name.toLowerCase()) {
+        return true;
+      }
+    }
+    return false;
+  }
+  setNavigatorRightButton(title){
     this.props.navigator.setButtons({
       rightButtons: [
         {
-          title:'搜索',
+          title:title,
           id: 'search',
         }
       ],
@@ -50,23 +99,161 @@ export default class SearchPage extends Component<{}> {
     });
   }
   onNavigatorEvent(event) {
-  if (event.type == 'NavBarButtonPress') {
-    if (event.id == 'search') {
-
+    if (event.id=='willDisappear') {
+      Keyboard.dismiss();
+    }
+    if (event.type == 'NavBarButtonPress') {
+      if (event.id == 'search') {
+        if (this.rightButtonText==='搜索') {
+          this.onLoad();
+          this.textInput.blur();
+          this.rightButtonText = '取消';
+        }else {
+          this.rightButtonText = '搜索';
+        }
+        this.setNavigatorRightButton(this.rightButtonText);
+      }
     }
   }
-}
+  getFavoriteKeys(){
+    this.favoriteDao.getFavoriteKeys()
+      .then(keys=>{
+        if (keys) {
+          this.setState({favoriteKeys:keys});
+        }
+        this.flushFavoriteState();
+      })
+      .catch(e=>{
+        this.flushFavoriteState();
+      })
+  }
+  //更新Project item 收藏状态
+  flushFavoriteState(){
+    let projectModels = [];
+    let items = this.items;
+    for (var i = 0; i < items.length; i++) {
+      projectModels.push(new ProjectModel(items[i],Utils.checkFavorite(items[i],this.state.favoriteKeys)));
+    }
+    this.setState({
+      data:projectModels,
+      refresh:false,
+    });
+  }
+  onLoad(){
+    this.setState({
+        refresh:true
+    });
+    let url = this.genURL(this.inputText);
+    fetch(url)
+      .then(response=>response.json())
+      .then(responseData=>{
+        this.setNavigatorRightButton('搜索');
+        if (!this||!responseData||!responseData.items||responseData.items.length===0) {
+          console.log('没有搜索到相关数据');
+          this.setState({
+            refresh:false
+          })
+        }else {
+          this.items = responseData.items;
+          this.getFavoriteKeys();
+          if (!this.checkKeyIsExist(this.keys,this.inputText)) {
+            this.setState({showBottomButton:true});
+          }
+        }
+      })
+      .catch(err=>{
+        this.setNavigatorRightButton('搜索');
+        this.setState({
+          refresh:false
+        })
+      })
+  }
+  genURL(text){
+    return API_URL+text+QUERY_STR;
+  }
+
+  _renderItem = (aa) => (
+      <RepositoryCell
+        data= {aa.item}
+        onFavorite={(item,isFavorite)=>{
+          if (isFavorite) {
+            this.favoriteDao.saveFavoriteItem(item.id.toString(),JSON.stringify(item));
+          }else {
+            this.favoriteDao.removeFavoriteItem(item.id.toString());
+          }
+        }}
+        onSelect={()=>{
+           this.props.navigator.push({
+             screen: 'com.fof.RepositoryDetail',
+             title: aa.item.item.full_name,
+             backButtonTitle:'',
+             passProps:{
+               item:aa.item.item,
+               isFavorite:aa.item.isFavorite,
+               flag:FLAG_STORAGE.flag_popular
+             },
+             navigatorStyle:{//此方式与苹果原生的hideWhenPushed一致
+                 tabBarHidden: true
+             }
+           });
+         }}
+       />
+    );
+  _keyExtractor = (projectModel, index) => index;
+
   render() {
+    let content = this.state.refresh?
+      <ActivityIndicator
+        style={styles.center}
+        size='large'
+        color='#2196F3'
+        animated={this.state.refresh}
+      />:<FlatList
+                data={this.state.data}
+                keyExtractor={this._keyExtractor}
+                renderItem={this._renderItem}
+                refreshing={this.state.refresh}
+              />;
+      let bottomButton = this.state.showBottomButton?
+                  <TouchableOpacity style={[styles.bottomButton,{backgroundColor:'#2196F3'}]}
+                    onPress={()=>{
+                      this.saveKey();
+                    }}
+                  >
+                    <View style={{justifyContent:'center'}}>
+                      <Text style={styles.title}>添加标签</Text>
+                    </View>
+                  </TouchableOpacity>:null;
     return (
       <View style={styles.container}>
-
+        {content}
+        {bottomButton}
       </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  center:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  title:{
+    fontSize:18,
+    fontWeight:'500',
+    color:'white'
+  },
+  bottomButton:{
+    alignItems:'center',
+    justifyContent:'center',
+    opacity:0.8,
+    height:40,
+    marginLeft:10,
+    marginRight:10,
+    marginBottom:5,
+    borderRadius:5
+  }
 });
--- app/screens/view/CustomSearchView.js
+++ app/screens/view/CustomSearchView.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import {
   StyleSheet,
   View,
-  TextInput
+  TextInput,
+  Dimensions
 } from 'react-native';
 
 export default class CustomSearchView extends Component {
@@ -14,11 +15,19 @@ export default class CustomSearchView extends Component {
   render() {
     return(
       <View style={styles.container}>
-        
+      <TextInput
+          ref='textInput'
+          placeholder='输入搜索关键字'
+          onChangeText={(text) => this.props.textChange(text,this.refs.textInput)}
+          onSubmitEditing={(text) => this.props.onSubmitEditing(text)}
+          style={styles.textInput}
+        />
       </View>
     );
   }
 }
+
+const window = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
@@ -26,4 +35,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
+  textInput:{
+    width: (window.width-120),
+    height: 30,
+    paddingLeft:5,
+    borderColor: 'white',
+    borderRadius:3,
+    opacity:0.7,
+    color:'white',
+    borderWidth: 1
+  }
 });
